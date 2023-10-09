335b38fa9b97017df0deaf607979c692ef0200e4
Antoon Prins <antoon.prins@surf.nl>
2023-10-09 20:57:48 +0200

    Add HttpCLient; Change Notifier name

---
+++ lib/HttpClient.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace OCA\RDMesh;
+
+use OCP\AppFramework\Http;
+
+class HttpClient
+{
+
+    /**
+     * @return mixed returns an array with in the following format:
+     *  [
+     *      'response' => the actual response in case of success, or a message string otherwise.
+     *      'success' => 'true' | 'false'
+     *  ]
+     */
+    public function curlPost(string $url, array $params = [])
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params, JSON_PRETTY_PRINT));
+        $output = json_decode(curl_exec($ch));
+        // \OC::$server->getLogger()->debug("curlPost output: " . print_r($output, true));
+        $info = curl_getinfo($ch);
+        // \OC::$server->getLogger()->debug(print_r($info, true));
+        curl_close($ch);
+        if ($output == false || $info['http_code'] != Http::STATUS_OK) {
+            $message = '';
+            if (isset($output->error)) {
+                $message .=  'Error: ' . $output->error . ". ";
+            }
+            if (isset($output->message)) {
+                $message .=  'Message: ' . $output->message . ". ";
+            }
+            if ($message == '') {
+                $message .= 'no error info';
+            }
+            \OC::$server->getLogger()->error("curlPost error: " . $message);
+            return ['message' => $message, 'success' => false];
+        } else {
+            return ['response' => $output, 'success' => true];
+        }
+    }
+}
---
+++ lib/Service/InvitationNotifier.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * 
+ */
+
+namespace OCA\RDMesh\Service;
+
+use OCP\Notification\INotification;
+use OCP\Notification\INotifier;
+
+class InvitationNotifier implements INotifier
+{
+
+    protected $factory;
+    private MeshService $meshService;
+
+    public function __construct(\OCP\L10N\IFactory $factory, MeshService $meshService)
+    {
+        $this->factory = $factory;
+        $this->meshService = $meshService;
+    }
+
+    /**
+     * Display the notification.
+     */
+    public function prepare(INotification $notification, $languageCode)
+    {
+        if ($notification->getApp() != 'notification-invite') {
+            throw new \InvalidArgumentException("Wrong app");
+        }
+
+        $l = $this->factory->get('notification', $languageCode);
+
+        switch ($notification->getSubject()) {
+                // Deal with known subjects
+            case 'invitation':
+                $notification->setParsedSubject(
+                    (string) $l->t(
+                        'You received an invitation from "%s" (token: "%s", sender domain: "%s").',
+                        [
+                            $notification->getSubjectParameters()[MeshService::PARAM_NAME_SENDER_EMAIL],
+                            $notification->getSubjectParameters()[MeshService::PARAM_NAME_TOKEN],
+                            $notification->getSubjectParameters()[MeshService::PARAM_NAME_SENDER_DOMAIN],
+                        ]
+                    )
+                );
+
+                foreach ($notification->getActions() as $action) {
+                    // display the buttons
+                    switch ($action->getLabel()) {
+                        case 'accept':
+                            // show the Accept label
+                            $action->setParsedLabel(
+                                (string) $l->t('Accept')
+                            );
+                            break;
+
+                        case 'decline':
+                            // show the Decline label
+                            $action->setParsedLabel(
+                                (string) $l->t('Decline')
+                            );
+                            break;
+                    }
+
+                    $notification->addParsedAction($action);
+                }
+                return $notification;
+                break;
+
+            default:
+                // Unknown subject => Unknown notification => throw
+                throw new \InvalidArgumentException();
+        }
+    }
+}
