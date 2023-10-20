2ba6f37d1ab199d1d8b1946732dd6a13bd57ba80
Antoon Prins <antoon.prins@surf.nl>
2023-10-20 14:10:21 +0200

    Add explanatory comment

--- tests/db-setup/config-values.sql
+++ tests/db-setup/config-values.sql
@@ -1,2 +1,4 @@
+-- The provider domain of this instance, equal to its trusted server domain value
+-- Eg. trusted server value is'https://rd-1.nl', then provider domain is 'rd-1.nl'
 INSERT INTO `oc_appconfig` (`appid`, `configkey`, `configvalue`) VALUES ('rd-mesh', 'domain', 'rd-1.nl') 
     ON DUPLICATE KEY UPDATE `configvalue`='rd-1.nl';
