e25bbd984cf6408ce692e4ff794c5c09b3c4b28c
Antoon Prins <antoon.prins@surf.nl>
2024-01-29 15:03:38 +0100

    Add translations

---
+++ l10n/en.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "invitation",
+    {
+        "ERROR_UNSPECIFIED": "An error has occurred",
+        "CREATE_INVITATION_NO_RECIPIENT_EMAIL": "Recipient email is required",
+        "CREATE_INVITATION_EMAIL_INVALID": "The recipient email is invalid"
+    },
+    "nplurals=2; plural=(n != 1);");
