382786e10c12e4c1dbf3c58a6f31c1b948a5f4dc
Antoon Prins <antoon.prins@surf.nl>
2024-05-24 09:38:10 +0200

    Ignore dev packages in release Clean up tests.sh script

--- tests/docker/integration-tests/tests.sh
+++ tests/docker/integration-tests/tests.sh
@@ -2,11 +2,7 @@
 
 # start the actual tests
 echo Start Invitation app integration tests
-echo sleeping 60s ... giving owncloud time to startup && sleep 60 &&
-
-# if this succeeds we know we have connectivity
-# curl -vv https://oc-1.nl/apps/invitation/registry/invitation-service-provider
-echo 
+echo waiting for 60s ... giving owncloud time to startup && sleep 60 &&
 echo
 echo "Starting integration unit tests"
 cd /tmp/tests/src
